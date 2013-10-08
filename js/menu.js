d2c8ff96b1ccf55b8e0da1038729c5bdf62cc80f
Ryan Tanner <mithranor@gmail.com>
2013-10-08 10:18:22 -0500

    Trying out some stuff in menu.js

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+var mCurrentIndex = 0;
+var request = new XMLHttpRequest();
+var mImages = new Array();
+var json;
+var path = '../data/menu.json';
+
+/*added*/
+request.open("GET",path, true);
+request.send();
+
+request.onreadystatechange = function(e)
+{
+	if(request.readyState === 4)
+	{
+		var jsonData = JSON.parse(request.responseText);
+		console.log(jsonData);
+
+		for (var i = 0, len = jsonData.menu.length; i < len; i++){
+			for(var j =0, len2 = jsonData.menu[i].length; j < len2; j++){}
+				var img = document.createElement("img");
+				img.setAttribute("src", jsonData.menu[i][j].imgPath);
+				document.body.appendChild(img); //adds the image to the document
+			}
+		}
+	}
+
+}
+
+
+
+$(document).ready( function() {
+	
+});
+
+window.addEventListener('load', function() {
+	console.log('window loaded');
+}, false);
