48bd70944badfc13a7cd7842cfd638a607b4272d
Robert Stewart <rcstewart@smu.edu>
2013-10-08 23:44:32 -0500

    changed how the tags were added
    The tags weren't being added correctly so css couldn't be applied

--- js/menu.js
+++ js/menu.js
@@ -18,18 +18,15 @@ request.onreadystatechange = function(e){
 		var myCake = $("#flavor div ul");
 
 		for (var i = 0, len = jsonData.menu.cakes.length; i < len; i++){
+				var listitem = document.createElement("li");
 				var img = document.createElement("img");
 				img.setAttribute("src", "artwork/" + jsonData.menu.cakes[i].img_url);
 				img.setAttribute("value", jsonData.menu.cakes[i].flavor);
 				//document.body.appendChild(img); //adds the image to the document
-				myCake.append('<li>');
-				myCake.append(img);
-				myCake.append('</li>');
+				listitem.appendChild(img);
+				myCake.append(listitem);
 			
 		}
-		// var img = document.createElement("img");
-		// img.setAttribute("src", jsonData.menu.cakes[0].img_url);
-		// document.body.appendChild(img);
 
 
 		var jsonData = JSON.parse(request.responseText);
@@ -38,19 +35,21 @@ request.onreadystatechange = function(e){
 		var myCake = $("#filling div ul");
 
 		for (var i = 0, len = jsonData.menu.fillings.length; i < len; i++){
+				var listitem = document.createElement("li");
+				var container = document.createElement("div");
+				var par = document.createElement("p");
 				var circle = document.createElement("div");
+				par.innerHTML = jsonData.menu.fillings[i].flavor;
 				circle.setAttribute("class", "circle");
 				circle.setAttribute("value", jsonData.menu.fillings[i].flavor);
 				var myId = "c";
 				myId = myId + jsonData.menu.fillings[i].flavor;
 				myId = myId.replace(/\s+/g,"");
 				circle.setAttribute("id", myId);
-				myCake.append('<li>');
-				myCake.append('<div>');
-				myCake.append(circle);
-				myCake.append(jsonData.menu.fillings[i].flavor);
-				myCake.append('</div>');
-				myCake.append('</li>');
+				container.appendChild(circle);
+				container.appendChild(par);
+				listitem.appendChild(container);
+				myCake.append(listitem);
 				$('#' + myId).css('background-color', jsonData.menu.fillings[i].rgb);
 				//document.getElementById(myId).style.bgcolor=jsonData.menu.fillings[i].rgb;
 				
@@ -63,13 +62,13 @@ request.onreadystatechange = function(e){
 		console.log(jsonData);
 		var myCake = $("#icing div ul");
 		for (var i = 0, len = jsonData.menu.frosting.length; i < len; i++){
+				var listitem = document.createElement("li");
 				var img = document.createElement("img");
 				img.setAttribute("src", "artwork/" + jsonData.menu.frosting[i].img_url);
 				img.setAttribute("value", jsonData.menu.frosting[i].flavor);
 				//document.body.appendChild(img); //adds the image to the document
-				myCake.append('<li>');
-				myCake.append(img);
-				myCake.append('</li>');
+				listitem.appendChild(img);
+				myCake.append(listitem);
 			
 		}
 
@@ -77,18 +76,19 @@ request.onreadystatechange = function(e){
 		console.log(jsonData);
 		var toppings = $("#topping div ul");
 		for (var i = 0, len = jsonData.menu.Toppings.length; i < len; i++){
+				var listitem = document.createElement("li");
+				var divitem = document.createElement("div");
+				var pitem = document.createElement("p");
 				var top = jsonData.menu.Toppings[i]
 				var checkbox = document.createElement("input");
 				checkbox.type = "checkbox";
 				checkbox.value = top;
 				checkbox.text = top;
-				toppings.append('<li>');
-				toppings.append('<div>');
-				toppings.append(checkbox);
-				
-				toppings.append(top);
-				toppings.append('</div>');
-				toppings.append('</li>');
+				pitem.innerHTML = top;
+				listitem.appendChild(divitem);
+				divitem.appendChild(checkbox);
+				divitem.appendChild(pitem);
+				toppings.append(listitem);
 			
 		}
 	}
