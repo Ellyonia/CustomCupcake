bba66975378eaa9e9eeecf543fe4c67cf4cde49e
Mithranor <mithranor@gmail.com>
2013-10-09 17:56:36 -0500

    changed order.html to order.php

---
+++ order.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Order</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet"  href="css/style.css" type="text/css">
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+			<script type="text/javascript" src='js/menu.js'></script>
+
+	</head>
+	<body>
+		<form id = 'order'>
+			<fieldset>
+				<legend>Order</legend>
+				<div>
+					<ul>
+
+					</ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'fav'>
+			<fieldset>
+				<legend>Favorites</legend>
+				<div>
+					<ul></ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'flavor'>
+			<fieldset>
+				<legend>Cupcake Flavor</legend>
+				<div>
+					<ul>
+					</ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'filling'>
+			<fieldset>
+				<legend>Filling</legend>
+				<div>
+					<ul>
+					</ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'icing'>
+			<fieldset>
+				<legend>Icing</legend>
+				<div>
+					<ul>
+					</ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'topping'>
+			<fieldset>
+				<legend>Toppings</legend>
+				<div>
+					<ul>
+					</ul>
+				</div>
+			</fieldset>
+		</form>
+	</body>
+</html>
