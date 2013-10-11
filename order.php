0b8f86691106b85efa19e222ab2a967f4f27a118
gcockrum <gcockrum@smu.edu>
2013-10-11 17:10:35 -0500

    back

---
+++ order.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Order</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet"  href="css/style.css" type="text/css">
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+			
+
+	</head>
+	<body>
+		<form id = 'order'>
+				<a>Order</a>
+				<div class="orderForm">
+					<ul class = "orderList">
+
+					</ul>
+				</div>
+				<div class = "submitOrder">
+					<input type="submit" value="Order" id="submitOrder" /> 
+				</div>
+		</form>
+		<form id = 'fav'>
+			<fieldset>
+				<legend>Favorites</legend>
+				<div>
+					<ul></ul>
+				</div>
+			</fieldset>
+		</form>
+		<form id = 'flavor'>
+				<a>Cupcake Flavor</a>
+				<div class='CFlavor'>
+					<ul>
+					</ul>
+				</div>
+		</form>
+		<form id = 'filling'>
+				<a>Filling</a>
+				<div class="CFilling">
+					<ul>
+					</ul>
+				</div>
+		</form>
+		<form id = 'icing'>
+				<a>Icing</a>
+				<div class='CIcing'>
+					<ul>
+					</ul>
+				</div>
+		</form>
+		<form id = 'topping'>
+				<a>Toppings</a>
+				<div class='CTopping'>
+					<ul>
+					</ul>
+				</div>
+				<div class="toppingClear">
+					<input type="button" value="Clear All Toppings" id="toppingClear" /> 
+				</div>
+		</form>
+		
+		<div class="reset">
+				<input type="submit" value="Reset Cupcake" id="reset" action = "" method = "post" name="reset"/> 
+		</div>
+		<form class="update">	
+			<input type="number" value="1" id="amount" /> 
+			<input type="button" value="Update Order" id="update" /> 
+		</form>
+			<input type="button" value="Add To Favorites" id="addtoFav" />
+
+	</body>
+	<script type="text/javascript" src='js/menu.js'></script>
+</html>
