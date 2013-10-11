aaea698a769623448ac8ec60cfb9981790be2568
gcockrum <gcockrum@smu.edu>
2013-10-11 04:21:17 -0500

    l

--- js/chart.js
+++ js/chart.js
@@ -18,15 +18,7 @@ request.onreadystatechange = function(e) {
 
 
 	if(request.readyState === 4) {
-// 		//Get the context of the canvas element we want to select
-// 		var ctx = document.getElementById("myChart").getContext("2d");
-// 		var myNewChart = new Chart(ctx).PolarArea(data);
-// 		//Get context with jQuery - using jQuery's .get() method.
-// var ctx = $("#myChart").get(0).getContext("2d");
-// //This will get the first returned node in the jQuery collection.
-// var myNewChart = new Chart(ctx);
-// new Chart(ctx).PolarArea(data,options);
-//var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
+
 		var jsonData = JSON.parse(request.responseText);
 		console.log(jsonData);
 		var data = jsonData.sales;
