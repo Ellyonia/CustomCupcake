d159ca1ab6a0813d987fbfbfa4c97caba08f9d0a
CaptainMustang8 <nmorris@smu.edu>
2013-10-11 01:05:22 -0500

    Updating sql file that reads in toppings bridge

--- ToppingsBridgetoDatabase.sql
+++ ToppingsBridgetoDatabase.sql
@@ -1,5 +1,10 @@
 /*reading ToppingsBridge csv file into table*/
 
-TRUNCATE TABLE 
+TRUNCATE TABLE CupcakeToppings;
+LOAD DATA LOCAL INFILE 'CustomCupcakesDBData-ToppingsBridge.csv'
+INTO TABLE CupcakeToppings
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+(Rn_ID, cupcake_ID, topping_ID);
 
 
