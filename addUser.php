f689f08d489a43c0d52793bfde0a15e02aac8bef
Ellyonia <ckeith@smu.edu>
2013-10-11 15:42:44 -0500

    Debugg test

--- addUser.php
+++ addUser.php
@@ -11,6 +11,7 @@
 	$zip = $_POST['zip'];
 	$ID = 0;
   session_start();
+  	echo $state;
 ?>
 
 <html>
@@ -40,8 +41,7 @@
 	$_SESSION['cID'] = $ID;
 	$sql2 = "INSERT INTO Customers VALUES ('$ID','$mailing','$fname','$lname','$address','$city','$state','$zip','$email','$pass','$phone')";
 	$result = mysql_query($sql) or die(mysql_error());
- 	header("Location: order.php");
+ 	//header("Location: order.php");
  	?>
  </body>
  </html>
- 
