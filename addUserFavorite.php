c8ab5111bef6584065b7634ad06d6a16e4910ce5
Kryss Linstromberg <dlinstromber@smu.edu>
2013-10-11 08:24:40 -0500

    uncomment

--- addUserFavorite.php
+++ addUserFavorite.php
@@ -1,10 +1,10 @@
 
 <?php
-    //include "ccAPI.php";
-    //$start = new ccAPI();
-    //echo "adlfkjad;lfj";
+   include "ccAPI.php";
+   $start = new ccAPI();
+    echo "adlfkjad;lfj";
     echo $_POST['test'];
-    //echo "adlfja;ldsjf";
-    //echo($start->addFavorites());
+    echo "adlfja;ldsjf";
+    echo($start->addFavorites());
 
 ?>
