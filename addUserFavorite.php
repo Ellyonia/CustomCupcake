deada1898db1cf5830ee693bfb695823fb1351dc
Robert Stewart <rcstewart@smu.edu>
2013-10-11 06:58:17 -0500

    ads

--- addUserFavorite.php
+++ addUserFavorite.php
@@ -3,7 +3,8 @@
     include "ccAPI.php";
     $start = new ccAPI();
     //echo "adlfkjad;lfj";
-    echo $_POST['test'];
+    //echo $_POST['test'];
+    echo "adlfja;ldsjf";
     //echo($start->addFavorites());
 
 ?>
