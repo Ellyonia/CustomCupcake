73d7706077b040f46faee15810d4c5fe3d96389e
gcockrum <gcockrum@smu.edu>
2013-10-11 03:33:06 -0500

    analit

--- analytics.php
+++ analytics.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet"  href="css/style.css" type="text/css">
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 			<script type="text/javascript" src='js/chart.js'></script>
-			<script src="js/chart.js-master/Chart.js"></script>
+			<script src="js/Chart.js-master/Chart.js"></script>
 
 	</head>
 	<body>
