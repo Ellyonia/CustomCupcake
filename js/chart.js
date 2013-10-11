9fb573f68770a0d96b55b4abc9577ffefc1fa714
gcockrum <gcockrum@smu.edu>
2013-10-11 08:18:46 -0500

    lol

--- js/chart.js
+++ js/chart.js
@@ -44,7 +44,7 @@ function FChart() {
      
             var jsonData = JSON.parse(request.responseText);
             console.log(jsonData);
-            var data = JSON.stringify(jsonData);
+            var data = jsonData.test;
             console.log(data);
             var ctx = document.getElementById("myChart2").getContext("2d");
 
