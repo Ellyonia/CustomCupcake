50f7ac0d6ba53e39e24ee9dc43ffb3f8af73007d
gcockrum <gcockrum@smu.edu>
2013-10-11 03:55:03 -0500

    analit

--- js/chart.js
+++ js/chart.js
@@ -7,15 +7,15 @@
 
 // // new Chart(ctx).PolarArea(data,options);
 
-// var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
-// request.open("GET",path, true);
-// request.send();
+var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
+request.open("GET",path, true);
+request.send();
 
-// request.onreadystatechange = function(e) {
+request.onreadystatechange = function(e) {
 
 
 
-// 	if(request.readyState === 4) {
+	if(request.readyState === 4) {
 // 		//Get the context of the canvas element we want to select
 // 		var ctx = document.getElementById("myChart").getContext("2d");
 // 		var myNewChart = new Chart(ctx).PolarArea(data);
@@ -24,9 +24,44 @@
 // //This will get the first returned node in the jQuery collection.
 // var myNewChart = new Chart(ctx);
 // new Chart(ctx).PolarArea(data,options);
-// 		var jsonData = JSON.parse(request.responseText);
-// 		console.log(jsonData);
-// 		var data = jsonData.menu.sales;
+var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
+		var jsonData = JSON.parse(request.responseText);
+		console.log(jsonData);
+		var data = jsonData.sales.cakes;
+//Get the context of the canvas element we want to select
+            var ctx = document.getElementById("myChart").getContext("2d");
+ 
+            //Create the data object to pass to the chart
+
+            
+            // var data = {
+            //     labels : ["January","February","March","April","May","June","July"],
+            //     datasets : [
+            //                 {
+            //                     fillColor : "rgba(220,220,220,0.5)",
+            //                     strokeColor : "rgba(220,220,220,1)",
+            //                     data : [65,59,90,81,56,55,40]
+            //                 },
+            //                 {
+            //                     fillColor : "rgba(151,187,205,0.5)",
+            //                     strokeColor : "rgba(151,187,205,1)",
+            //                     data : [28,48,40,19,96,27,100]
+            //                 }
+            //                ]
+            //           };
+ 
+            //The options we are going to pass to the chart
+            options = {
+                barDatasetSpacing : 15,
+                barValueSpacing: 10
+            };
+ 
+            //Create the chart
+            new Chart(ctx).Bar(data, options);
+        }
+
+	}
+}
 // 		new Chart(ctx).PolarArea(data,{
 // 	//Boolean - Whether we should show a stroke on each segment
 // 	segmentShowStroke : true,
@@ -69,6 +104,8 @@ $(document).ready(function() {
             var ctx = document.getElementById("myChart").getContext("2d");
  
             //Create the data object to pass to the chart
+
+            var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
             var data = {
                 labels : ["January","February","March","April","May","June","July"],
                 datasets : [
