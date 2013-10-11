aa75d7bae5d6a83fef625b5983a064be200edf63
gcockrum <gcockrum@smu.edu>
2013-10-11 06:39:46 -0500

    mhm

--- js/chart.js
+++ js/chart.js
@@ -1,34 +1,49 @@
 $(document).ready(function() {
 
-var request = new XMLHttpRequest();
-var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
-request.open("GET",path, true);
-request.send();
+    var request = new XMLHttpRequest();
+    var path = 'http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/CustomCupcake/data/sales.json';
+    request.open("GET",path, true);
+    request.send();
 
-request.onreadystatechange = function(e) {
+    request.onreadystatechange = function(e) {
 
-	if(request.readyState === 4) {
-		var jsonData = JSON.parse(request.responseText);
-		console.log(jsonData);
-		var toppingsChart = jsonData.salesToppings;
-        var flavorChart = jsonData.salesFlavor;
-        var ctx = document.getElementById("myChart").getContext("2d");
- 
-        //The options we are going to pass to the chart
-        options1 = {
-            barDatasetSpacing : 15,
-            barValueSpacing: 10,
-            barStrokeWidth: 15
-        };
+    	if(request.readyState === 4) {
 
-        // options2 = {
+            function TChart() {
+            	var jsonData = JSON.parse(request.responseText);
+            	console.log(jsonData);
+            	var toppingsChart = jsonData.salesToppings;
+                var ctx = document.getElementById("myChart").getContext("2d");
 
-        // };
+                //The options we are going to pass to the chart
+                options1 = {
+                    barDatasetSpacing : 15,
+                    barValueSpacing: 10,
+                    barStrokeWidth: 15
+                };
 
-        //Create the chart
-        new Chart(ctx).Bar(toppingsChart, options1);
-        new Chart(ctx).Pie(flavorChart);
+                // options2 = {
+
+                // };
+
+                //Create the chart
+                new Chart(ctx).Bar(toppingsChart, options1);
+               
+            }
+
+            function FChart() {
+                var jsonData = JSON.parse(request.responseText);
+                console.log(jsonData);
+                var flavorChart = jsonData.salesFlavor;
+                var ctx = document.getElementById("myChart").getContext("2d");
+
+                 new Chart(ctx).Pie(flavorChart);
+
+            }
         }
 
-	}
+    }
+
+    TChart();
+    FChart();
 });
