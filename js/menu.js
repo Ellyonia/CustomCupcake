853d63f5b5aa154b01d2b121807d89d00d8881f3
Robert Stewart <rcstewart@smu.edu>
2013-10-10 20:47:00 -0500

    testing on click

--- js/menu.js
+++ js/menu.js
@@ -129,5 +129,23 @@ function onMouseDown(e){
 
 window.addEventListener('load', function() {
 	console.log('window loaded');
+
+	var myimgs = document.body.getElementsByTagName("img");
+		for (var i=0; i<myimgs.length; i++) {
+   			myimgs[i].onclick = function (e) {
+   		var temps = document.body.getElementsByTagName("img");
+   		// for (var i = 0; i<4; i++) {
+   		// 	temps[i].className = "";
+   		// }
+		this.className = "selected";
+		return false;
+   	};
+
+	}, false);
+	
+}, false);
+
+window.addEventListener('load', function() {
+	console.log('window loaded');
 }, false);
 
