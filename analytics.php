273f4e017a764aa74c54afeded58037df0000772
gcockrum <gcockrum@smu.edu>
2013-10-11 03:27:05 -0500

    analit

--- analytics.php
+++ analytics.php
@@ -3,13 +3,14 @@
 	<head>
 		<title>Analytics</title>
 		<meta charset="UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet"  href="css/style.css" type="text/css">
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 			<script type="text/javascript" src='js/chart.js'></script>
 			<script src="Chart.js"></script>
 
 	</head>
-	<body>
+	<body onload="createChart();">
 		<canvas id="myChart" width="700" height="400"></canvas>
 		<header>stuff</header>
 	</body>
