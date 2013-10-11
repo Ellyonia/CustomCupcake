ca417cfc9a78677689f07bf5108c7c43050efd84
Robert Stewart <rcstewart@smu.edu>
2013-10-11 06:38:22 -0500

    asdadsf

--- js/menu.js
+++ js/menu.js
@@ -258,17 +258,15 @@ var request = new XMLHttpRequest();
 				
 			if(request.readyState === 4)
 			{
-		  	myAjax = new Ajax.Request(url, {
-        	method: 'post',
-        	data: "jsonArr=" + JSON.stringify(json),
-        	contentType: 'application/x-www-form-urlencoded',
-        	onComplete: function(e){
-            if (200 == e.status) {
-                result = e.responseText;
-                console.log(result);
-            }
-        }
-    });
+		  	$.ajax({
+  			type: "POST",
+  			dataType: "json",
+  			data: {test : worked},
+  			url: 'addUserFavorite.php',
+  			success: function(data) {
+    		alert(data);
+  }
+});
 			}
 		}
 		});
