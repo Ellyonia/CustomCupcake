cb89d6157a5446a579b0cd681d87895182fce30b
gcockrum <gcockrum@smu.edu>
2013-10-11 03:47:45 -0500

    analit

--- js/chart.js
+++ js/chart.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 	// body...
 
 
-function createChart()
+// function createChart()
         {
             //Get the context of the canvas element we want to select
             var ctx = document.getElementById("myChart").getContext("2d");
